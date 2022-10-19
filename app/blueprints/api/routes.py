d230bad51ea406173fcd6178094072854e857c88
Jessica Kulas <jessrevelette@gmail.com>
2022-10-19 14:53:59 -0400

    updated

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,3 +1,13 @@
 from flask import Flask, render_template
+import requests
+import json
+
+def call_API(foodName, apiKey):
+    url = f'https://api.nal.usda.gov/fdc/v1/foods/search?api_key={apiKey}&query={foodName}'
+    r = requests.get(url)
+    print(r.status_code) 
+    return r.json
+
+
 
 
